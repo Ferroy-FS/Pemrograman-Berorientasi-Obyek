de27450c358afd37350fe85ebf8db5302eb26921
Ferroy-FS <nando007seroy@gmail.com>
2025-08-27 17:53:57 +0700

    again, try recommit (insya allah final)

---
+++ Kakek.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbopertemuan1;
+
+/**
+ *
+ * @author LEGION
+ */
+public class Kakek {
+
+    protected String getWarnaMata() {
+        return warnaMata;
+    }
+    protected void setWarnaMata(String warnaMata) {
+        this.warnaMata = warnaMata;
+    }
+
+    protected String getWarnaRambut() {
+        return warnaRambut;
+    }
+    protected void setWarnaRambut(String warnaRambut) {
+        this.warnaRambut = warnaRambut;
+    }
+
+    protected int getTinggiBadan() {
+        return tinggiBadan;
+    }
+    protected void setTinggiBadan(int tinggiBadan) {
+        this.tinggiBadan = tinggiBadan;
+    }
+
+    protected String getNama() {
+        return nama;
+    }
+    protected void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    protected int getUmur() {
+        return Umur;
+    }
+    protected void setUmur(int Umur) {
+        this.Umur = Umur;
+    }
+
+    private String warnaMata;
+    private String warnaRambut;
+    private int tinggiBadan;
+    private String nama;
+    private int Umur;
+}
